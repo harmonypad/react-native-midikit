fdcf3cf560e4d51ce68bc280921439382f8e6f01
purcymarte <purcymarte@gmail.com>
2023-06-06 19:11:24 +0200

    chore: basic ui components for note on/off in example project

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { multiply } from 'react-native-midikit';
 
+import Pad from './Pad';
+
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
@@ -10,9 +12,17 @@ export default function App() {
     multiply(3, 7).then(setResult);
   }, []);
 
+  const PAD_COUNT = 12;
+  const pads: number[] = Array.from(Array(PAD_COUNT), (_, i) => i);
+
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <View style={styles.instrument}>
+        {pads.map((_, i: number) => {
+          return <Pad id={i} />;
+        })}
+      </View>
     </View>
   );
 }
@@ -27,5 +37,13 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  instrument: {
+    flex: 1,
+    flexDirection: 'row',
+    height: 100,
+    maxHeight: 240,
+    width: '100%',
+    backgroundColor: 'yellow',
   },
 });
