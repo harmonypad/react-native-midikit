0838b09dffb7d7277adb05dd41b3fc1b4ee9000f
purcymarte <purcymarte@gmail.com>
2023-07-26 17:54:08 +0200

    test: basic unit test for access class, based on mocked response from native module

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,29 @@
-it.todo('write a test');
+// Import the class to be tested
+import { ReactNativeMidiKitAccess } from '../';
+
+// Mock 'NativeModules'
+jest.mock('react-native', () => ({
+  Platform: {
+    select: jest.fn().mockReturnValue(null), // Mock the 'select' method to return null
+  },
+  NativeModules: {
+    Midikit: {
+      start: jest.fn().mockResolvedValue(null), // Mock the async method to return a null promise
+    },
+  },
+}));
+
+// Describe the test suite
+describe('ReactNativeMIDIKit', () => {
+  // Test case: Check if the return value of getEndpoints is null
+  it('should return null from getEndpoints', async () => {
+    // Create an instance of ReactNativeMIDIKit
+    const midiKitInstance = new ReactNativeMidiKitAccess();
+
+    // Call the async method and await the result
+    const result = await midiKitInstance.connect();
+
+    // Assert that the return value is null
+    expect(result).toBeNull();
+  });
+});
